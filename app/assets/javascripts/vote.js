10057f5e548c8bcc4caf42bc485dc9367ec40e71
Sarah Griffis <sgriffis@paperlesspost.com>
2015-02-12 22:05:55 -0500

    CSS refactor on vote bottons and form refactor.

---
+++ app/assets/javascripts/vote.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+  $('.collection_radio_buttons').hover(function(){
+     $(this).css("background-color", "#B2CFE2");
+      }, function(){
+      $(this).css("background-color", "#bdc3c7");
+  });
+
+//  $(":checked").parents('label').css({
+//    'border-color' : 'black',
+//    'border-style' : 'solid'
+//  });
+
+});
