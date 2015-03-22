4bc8552f8099d226c490fd9675982ec9a3eb1011
Sarah Griffis <sgriffis@paperlesspost.com>
2015-03-22 12:23:58 -0400

    Infinite scroll fix and layout changes.

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -3,7 +3,8 @@ $( document ).ready(function() {
     $(window).on ("scroll", function() {
       more_posts_url = $('.pagination .next_page').attr('href');
       console.log(more_posts_url)
-      if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 100){
+      if (more_posts_url && $(window).scrollTop() <= $(document).height() - $(window).height() - 50){
+      //if ($win.height() + $win.scrollTop() > (getDocumentHeight()-10))
         $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />')
         $.getScript(more_posts_url)
         return
