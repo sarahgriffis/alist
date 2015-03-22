5e7285f502ce3d57b2539c3449fdb4261e9fa503
Sarah Griffis <sgriffis@paperlesspost.com>
2015-03-22 12:32:48 -0400

    more infinite scroll and layout changes

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -9,6 +9,13 @@ $( document ).ready(function() {
         $.getScript(more_posts_url)
         return
       };
+      if (more_posts_url && $(window).scrollTop() >= $(document).height() - $(window).height() - 10){
+      //if ($win.height() + $win.scrollTop() > (getDocumentHeight()-10))
+        $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />')
+        $.getScript(more_posts_url)
+        return
+      };
+
     });
     return
   };
