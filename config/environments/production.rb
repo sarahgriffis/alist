f1156dc48c30846da28df01a4e69834ebb392247
Sarah Griffis <sgriffis@paperlesspost.com>
2015-04-09 18:28:43 -0400

    fixing typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,13 +67,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-  address: “smtp.gmail.com”,
+  address: 'smtp.gmail.com',
   port: 587,
-  domain: ENV[“GMAIL_DOMAIN”],
-  authentication: “plain”,
+  domain: ENV['GMAIL_DOMAIN'],
+  authentication: 'plain',
   enable_starttls_auto: true,
-  user_name: ENV[“GMAIL_USERNAME”],
-  password: ENV[“GMAIL_PASSWORD”]
+  user_name: ENV['GMAIL_USERNAME'],
+  password: ENV['GMAIL_PASSWORD']
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
