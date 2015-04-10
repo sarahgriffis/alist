42aaa1107b7e8e14b75b1896e2c49a09789b9a88
Sarah Griffis <sgriffis@paperlesspost.com>
2015-04-09 21:49:16 -0400

    mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'thealist.io' }
 
   config.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
